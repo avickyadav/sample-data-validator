adbeeda1ab68940f8f01bb82631ce2cdbfdcfc74
avickyadav <64485057+avickyadav@users.noreply.github.com>
2024-11-18 13:37:12 +0530

    added via uploads

---
+++ constants/rialto_table_constants.py
@@ -0,0 +1,35 @@
+rialto_table_necessary_columns = [
+    "CNT Number",
+    "Manufacturer",
+    "Model or Part Number",
+    "Category",
+    "Device Type",
+    "OEM Family",
+    "Parent Asset Serial",
+    "Serial Number",
+    "Address 1",
+    "Address 2",
+    "Address 3",
+    "City",
+    "State/Province",
+    "Postal Code",
+    "Country",
+    "Support Level",
+    "Coverage SLA ",
+    "Coverage Start Date",
+    "Coverage End Date",
+    "Extended Price"
+]
+
+rialto_table_fields_with_optional_values = [
+    "Address 1",
+    "Address 2",
+    "Address 3",
+    "Coverage Start Date",
+    "Coverage End Date",
+    "Extended Price",
+    "Postal Code",
+    "Serial Number",
+    "State/Province",
+    "City"
+]
---
+++ constants/ship_method.py
@@ -0,0 +1,16 @@
+cds_entity_to_subsidiary = {
+    'APAC': 3,
+    'DACH': 6,
+    'UK': 7,
+    'Canada': 8,
+    'US': 10
+}
+
+
+subsidiary_to_ship_method = {
+    3: 12305,
+    6: 12304,
+    7: 27041,
+    8: 12303,
+    10: 27065
+}
